406f7256c1a9ab3c049d698a8904725a465f0283
Merry Damsko <m.d4msk0@gmail.com>
2022-11-01 00:40:58 +0100

    2D Perlin

--- sketch.js
+++ sketch.js
@@ -7,17 +7,24 @@ function setup() {
 }
 
 function draw() {
+
+  var yoff = 0;
+
   loadPixels();
-  for (var x = 0; x < width; x++) {
-    for (var y = 0; y < height; y++) {
+  for (var y = 0; y < height; y++) {
+    var xoff = 0;
+    for (var x = 0; x < width; x++) {
       var index = (x + y * width) * 4;
-      var r = random(255);
+      var r = noise(xoff, yoff) * 255;
       pixels[index + 0] = r;
       pixels[index + 1] = r;
       pixels[index + 2] = r;
       pixels[index + 3] = 255;
 
+      xoff += inc;
     }
+    yoff += inc;
   }
+  
   updatePixels();
 }
