69a1415a43f6baca8379bce8f3acc0c485b065da
Merry Damsko <m.d4msk0@gmail.com>
2022-11-02 07:57:39 +0100

    2d flowfield

--- sketch.js
+++ sketch.js
@@ -8,43 +8,56 @@ var fr;
 
 var particles = [];
 
+var flowfield;
+
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(500, 500);
   pixelDensity(1);
   cols = floor(width / scl);
   rows = floor(height / scl);
   fr = createP('');
 
-  for (var i = 0; i < 100; i++) {
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 1000; i++) {
     particles[i] = new Particle();
   }
+  background(255);
 
 }
 
 function draw() {
-  background(255);
+  
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      var angle = noise(xoff, yoff, zoff) * TWO_PI;
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
       var v = p5.Vector.fromAngle(angle);
+      v.setMag(0.5);
+      flowfield[index] = v;
       xoff += inc;
-      stroke(0);
-      push();
-      translate(x * scl, y * scl);
-      rotate(v.heading());
-      line(0, 0, scl, 0);
-      pop();
+      stroke(0, 50);
+      strokeWeight(1);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // line(0, 0, scl, 0);
+      // pop();
     }
     yoff += inc;
 
-    zoff += 0.001;
+    zoff += 0.0003;
   }
 
   for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
     particles[i].update();
-    partices[i].show();
+    particles[i].edges();
+    particles[i].show();
+    
+    
   }
 
 
